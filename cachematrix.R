4f34bd31c24220134262f2199b8a300959d310b1
Amine Bennani <bennaniamine255@gmail.com>
2014-11-23 22:22:53 +0100

    22h22 working good, comments not finished

--- cachematrix.R
+++ cachematrix.R
@@ -6,7 +6,16 @@
 ##makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL                                        #initialize the inverse to NULL at every call of makeCacheMatrix
+        set <- function(y) {                  
+                x <<- y                                    #setting the value of x
+                inv <<- NULL                               #as a new object x is ed, the value of inv has to be reset to NULL
+        }
+        get <- function() x                                #return the value of the matrix x that we want to inverse
+        setinverse <- function(inverse) inv <<- inverse    #store the value of the inverse in cache
+        getinverse <- function() inv                       #return the value of the inverse stored in the cache
+        
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
@@ -14,4 +23,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()                         #get the value of the inverse from the object x
+        if(!is.null(inv)) {                           #if the inverse is not NULL then we get it from cache
+                message("getting cached data")
+                return(inv)                           #the inverse stored in cache is returned
+        }                                        
+        data <- x$get()                               #if the inverse is not in the cache (inv=NULL), then we get the value of the matrix
+        inv <- solve(data, ...)                       #and the inverse is calculated
+        x$setinverse(inv)                             #then inverse is stored in cache
+        inv                                           #finally the inverse is returned
 }
+
+
